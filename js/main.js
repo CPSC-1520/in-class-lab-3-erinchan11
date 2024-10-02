40e4acb34626c1b2c658942509bbdd705165ec76
erinchan11 <98192301+erinchan11@users.noreply.github.com>
2024-10-01 22:34:40 -0600

    Add JS Code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-// Enter your code below.
+// 10/01/24 //
+// In-Class Lab 3 - Erin Chan //
+
+//Variables//
+let resourceList = document.querySelector('.javascript-resources')
+let showButton = document.getElementById('show-resources')
+
+//Exercise 1//
+showButton.addEventListener("click", removeDNone);
+function removeDNone()
+{
+    resourceList.classList.remove('d-none');
+}
+
+//Exercise 2//
+resourceList.addEventListener("mouseover", (eventBold)=>
+{
+    eventBold.target.classList.add("fw-bold")
+})
+
+//Exercise 3//
+resourceList.addEventListener("mouseout", (eventUnBold)=> 
+{
+    eventUnBold.target.classList.remove("fw-bold")
+})
+
+//Exercise 4//
+resourceList.addEventListener("click", (eventItalics)=>
+{
+    eventItalics.target.classList.add("fst-italic")
+})
